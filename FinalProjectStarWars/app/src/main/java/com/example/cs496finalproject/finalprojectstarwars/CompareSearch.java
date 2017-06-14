7cc568d325109517ecdcbd8bb1608e4bf8705127
chasecoltman <chase.coltman@gmail.com>
2017-06-13 18:29:22 -0700

    Added Settings Wheel
    I added the settings wheel button to the top left side of the
    activity_compare_results.xml file. When the wheel is clicked, the user
    is sent to the prefs.xml where they are able to type in their search
    queries. One issue however is that when you hit the back button, it
    sends the user all the way back to the main screen. I also added in a
    button back on the activity_compare_results.xml to due manual refreshing
    until we can get it to automatically refresh.

--- FinalProjectStarWars/app/src/main/java/com/example/cs496finalproject/finalprojectstarwars/CompareSearch.java
+++ FinalProjectStarWars/app/src/main/java/com/example/cs496finalproject/finalprojectstarwars/CompareSearch.java
@@ -1,10 +1,13 @@
 package com.example.cs496finalproject.finalprojectstarwars;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 
 /**
  * Created by Godtop on 6/6/2017.
@@ -24,7 +27,23 @@ public class CompareSearch extends AppCompatActivity  {
         //mCompareSearchResultsRV.setLayoutManager(new LinearLayoutManager(this));
         //mCompareSearchResultsRV.setHasFixedSize(true);
         //mCompareSearchResultsRV.setAdapter(adapter);
-
     }
 
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.settings_wheel, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Intent settingsIntent = new Intent(this, SettingsActivity.class);
+                startActivity(settingsIntent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
+
